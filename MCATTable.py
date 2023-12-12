d83c05fd3dc4617b9c08ab01288a2d020f0181f2
AndrewCowanTC <andrew.cowan@tc.gc.ca>
2023-12-12 07:00:40 -0500

    First Upload
    Added the three files.  MCAT_File includes logic to determine amplitudes, MCATTable includes tables of tolerances, and ToleranceTable organizes the results as per the tables in the CFR 213 Appendix D

---
+++ MCATTable.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 24 12:01:43 2023
+
+@author: Andrew Cowan
+"""
+
+
+import MCAT_File
+from tabulate import tabulate
+
+class MCATTable:
+    
+    def __init__(self):
+        self.Cants = [5]
+        self.Gauges = [56.5,57]
+        self.Chords = [31,62,124]
+        self.Classes = [6,7,8,9]
+        self.Country = "US"
+        
+        
+    def GenerateTable(self):
+        
+        for el in self.Cants:
+            for g in self.Gauges:
+                if el == 0 and g == 57: continue
+                for ch in self.Chords:
+                    Parameters = self.PrintTableSetup()
+                    Headers = [""]
+                    for c in self.Classes:
+
+                        # print("Gauge: ",g," Class: ",c," Chord: ",ch," Cant: ",el)
+                        Headers.append("Class: "+str(c))
+                        MCAT = MCAT_File.MCAT_Class(c,ch,g,el,self.Country)
+                        Parameters[0].append(MCAT.a2())
+                        Parameters[1].append(MCAT.a3())
+                        Parameters[2].append(MCAT.a4())
+                        Parameters[3].append(MCAT.a5())
+                        Parameters[4].append(MCAT.a6())
+                        Parameters[5].append(MCAT.a7())
+                        Parameters[6].append(MCAT.a8())
+                        Parameters[7].append(MCAT.a9())
+                        Parameters[8].append(MCAT.a10())
+                        Parameters[9].append(MCAT.a11())
+                        Parameters[10].append(MCAT.a13())
+                        
+                    self.PrettyPrint(Parameters,c,ch,g,el,Headers)
+    
+    def PrintTableSetup(self):
+        
+        Parameters = []
+        for i in range(12):
+            if i != 10:
+                Parameters.append([["a"+str(i+2)]])
+
+        return Parameters
+    
+    def PrettyPrint(self,Parameters,Class,Chord,Gauge,Cant,Headers):
+        
+        print("Gauge: ",Gauge," Chord: ",Chord," Cant: ",Cant)
+        print(tabulate(Parameters, tablefmt="grid",headers = Headers))
+
+if __name__ == "__main__":
+    Table = MCATTable()
+    Table.GenerateTable()
---
+++ MCAT_File.py
@@ -0,0 +1,240 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 24 08:23:27 2023
+
+@author: Andrew Cowan
+"""
+
+import ToleranceTables
+
+class MCAT_Class:
+    
+    def __init__(self,Class,Chord,Gauge,Cant,Country):
+        self.Cant = Cant
+        self.Gauge = Gauge
+        self.Chord = Chord
+        self.Class = Class
+        self.Country = Country
+        self.Table = ToleranceTables.TSRTable(self.Country)
+        
+        if self.Cant > 5:
+            self.HighCant = True
+            self.Cant = 5
+        else:
+            self.HighCant = False
+        
+        self.GaugeTable = self.Table.GaugeTable()
+        self.AlinementTable = self.Table.AlinementTable()
+        self.SurfaceTable = self.Table.SurfaceTable()
+        self.WarpTable = self.Table.WarpTable()
+        
+        self.GaugeMax = self.SearchTable(self.GaugeTable,"GaugeMax","Class",self.Class)
+        self.GaugeMin = self.SearchTable(self.GaugeTable,"GaugeMin","Class",self.Class)
+        self.GaugeDelta = self.SearchTable(self.GaugeTable,"GaugeDelta","Class",self.Class)
+        self.Alinement = self.SearchTable(self.AlinementTable,"Tolerance","Class",self.Class,"Chord",self.Chord,"Cant",self.Cant)
+        self.Surface = self.SearchTable(self.SurfaceTable,"Tolerance","Class",self.Class,"Chord",self.Chord,"Cant",self.Cant)
+        self.Warp = self.SearchTable(self.WarpTable,"Tolerance","Class",self.Class,"Cant",self.Cant)
+    
+    def SearchTable(self,Table,Request,Searchable1,Condition1,Searchable2=None,Condition2=None,Searchable3=None,Condition3=None):
+        
+        for Dict in Table:
+            if Dict[Searchable1] == Condition1:
+                if Searchable2 == None:
+                    return Dict.get(Request)
+                elif Dict[Searchable2] == Condition2:
+                    if Searchable3 == None:
+                        return Dict.get(Request)
+                    elif Dict[Searchable3] == Condition3:
+                        return Dict.get(Request)
+                    
+    def a2(self):
+        
+        if self.Class >= 6:
+            if self.Chord == 31 and self.Gauge - self.GaugeMin > self.GaugeDelta:
+                a2 = self.GaugeDelta
+            else:
+                a2 = self.Gauge - self.GaugeMin   
+            if a2 > self.Alinement:
+                a2 = self.Alinement
+            return a2
+        else: #Class 5 (US/CA) and below calculations
+            if self.Gauge - self.GaugeMin > self.Alinement:
+                return self.Alinement
+            else:
+               return self.Gauge - self.GaugeMin
+            
+    def a3(self):
+        #a3 - Gauge Widdening
+        
+        if self.Class >= 6:
+            if self.Chord == 31 and self.GaugeMax - self.Gauge > self.GaugeDelta:
+                a3 = self.GaugeDelta
+            else:
+                a3 = self.GaugeMax - self.Gauge
+            if self.GaugeMax - self.Gauge > self.Alinement:
+                a3 = self.Alinement
+            return a3
+        else: #Class 5 and below calculations
+            if self.GaugeMax - self.Gauge > self.Alinement:
+                return self.Alinement
+            else:
+               return self.GaugeMax - self.Gauge
+    
+    def a4(self):
+        #a4 - Repeated Alinement
+        if self.Class >= 6:
+            AlinementRepeatTable = self.Table.AlinementRepeatTable()
+            return self.SearchTable(AlinementRepeatTable,"Tolerance","Class",self.Class,"Chord",self.Chord)
+        else: #Class 5 --- NOTE: Alignment tolerance applied downwards across N/A
+            return self.Alinement
+        
+    def a5(self):
+        #a5 - Single Alinement
+        AlinementTable = self.Table.AlinementTable()
+        
+        return self.SearchTable(AlinementTable,"Tolerance","Class",self.Class,"Chord",self.Chord,"Cant",self.Cant)
+    
+    def a6(self):
+        #a6 - Single Alinement
+        if self.Class >=6:
+            if self.Alinement + self.Gauge > self.GaugeMax:
+                a6 = self.Gauge + self.Alinement - self.GaugeMax
+            else:
+                a6 = 0
+            if self.Chord == 31 and self.Alinement > self.GaugeDelta and self.Alinement-self.GaugeDelta > a6:
+                a6 = self.Alinement-self.GaugeDelta
+            return a6
+        else:
+            if self.Alinement + self.Gauge > self.GaugeMax:
+                return self.Gauge + self.Alinement - self.GaugeMax
+            else:
+                return 0
+    
+    def a9(self):
+        #a4 - Repeated Surface
+        if self.Class >= 6:
+            SurfaceRepeatTable = self.Table.SurfaceRepeatTable()
+            return self.SearchTable(SurfaceRepeatTable,"Tolerance","Class",self.Class,"Chord",self.Chord)
+        else:
+            return self.SearchTable(self.SurfaceTable,"Tolerance","Class",self.Class,"Chord",self.Chord,"Cant",self.Cant)
+        
+    def a10(self):
+        #a10 - Single Surface
+        return self.SearchTable(self.SurfaceTable,"Tolerance","Class",self.Class,"Chord",self.Chord,"Cant",self.Cant)
+    
+    def a11(self):
+        #a10 - Single Surface
+        if self.Class >= 6:
+            if self.Surface > self.Warp and self.Chord <= 62:
+                a11 = self.Surface - self.Warp
+            else:
+                a11 = 0
+            return a11
+        else:
+            Cant = 6 if self.HighCant else 5
+            Warp = self.SearchTable(self.WarpTable,"Tolerance","Class",self.Class,"Cant",Cant)
+            if self.Surface > Warp and self.Chord <= 62:
+                a11 = self.Surface - Warp
+            else:
+                a11 = 0
+            return a11
+    
+    def a7(self):
+        if self.Class == 9 or self.HighCant:
+            a7 = (2/3)*self.a5()
+            return a7
+        else:
+            return None
+    
+    def a8(self):
+        if self.Class == 9 or self.HighCant:
+            if self.a6() > 0:
+                a8 = self.a7() - (self.a5() - self.a6())
+                if a8>0:
+                    return a8
+                else:
+                    return 0
+            else:
+                return 0
+        else:
+            return None
+        
+    def a13(self):
+        if self.Class == 9 or self.HighCant:
+            a13 = (2/3)*self.a10()
+            return a13
+        else:
+            return None
+        
+if __name__ == "__main__":
+    #Test cases for individual amplitudes
+    Cant = [0,3,5]
+    TrackType = "Tanget"
+    Gauge = [56.5,57]
+    Class=[6,7,8,9]
+    Chord = [31,62,124]
+    Country = "US"
+
+    # a2
+    # for el in Cant:
+    #     for g in Gauge:
+    #         for c in Class:
+    #             for ch in Chord:
+    #                 print("Gauge: ",g," Class: ",c," Chord: ",ch," Cant: ",el)
+    #                 print(MCAT_Class(c,ch,g,el,Country).a2())
+    
+    # a3
+    # for el in Cant:
+    #     for g in Gauge:
+    #         for c in Class:
+    #             for ch in Chord:
+    #                 print("Gauge: ",g," Class: ",c," Chord: ",ch," Cant: ",el)
+    #                 print(MCAT_Class(c,ch,g,el,Country).a3())
+    
+    # a4
+    # for el in Cant:
+    #     for g in Gauge:
+    #         for c in Class:
+    #             for ch in Chord:
+    #                 print("Gauge: ",g," Class: ",c," Chord: ",ch," Cant: ",el)
+    #                 print(MCAT_Class(c,ch,g,el,Country).a4())
+    
+    #a5
+    # for el in Cant:
+    #     for g in Gauge:
+    #         for c in Class:
+    #             for ch in Chord:
+    #                 print("Gauge: ",g," Class: ",c," Chord: ",ch," Cant: ",el)
+    #                 print(MCAT_Class(c,ch,g,el,Country).a5())
+    
+    #a6
+    # for el in Cant:
+    #     for g in Gauge:
+    #         for c in Class:
+    #             for ch in Chord:
+    #                 print("Gauge: ",g," Class: ",c," Chord: ",ch," Cant: ",el)
+    #                 print(MCAT_Class(c,ch,g,el,Country).a6())
+    
+    #a9
+    # for el in Cant:
+    #     for g in Gauge:
+    #         for c in Class:
+    #             for ch in Chord:
+    #                 print("Gauge: ",g," Class: ",c," Chord: ",ch," Cant: ",el)
+    #                 print(MCAT_Class(c,ch,g,el,Country).a9())
+    
+    #a10
+    # for el in Cant:
+    #     for g in Gauge:
+    #         for c in Class:
+    #             for ch in Chord:
+    #                 print("Gauge: ",g," Class: ",c," Chord: ",ch," Cant: ",el)
+    #                 print(MCAT_Class(c,ch,g,el,Country).a10())
+    
+    #a11
+    for el in Cant:
+        for g in Gauge:
+            for c in Class:
+                for ch in Chord:
+                    print("Gauge: ",g," Class: ",c," Chord: ",ch," Cant: ",el)
+                    print(MCAT_Class(c,ch,g,el,Country).a11())
